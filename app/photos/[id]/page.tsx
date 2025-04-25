5f70b031041516cf8d01aa30fa79b06e38299b28
dark9wesley <pengyw97@gmail.com>
2025-04-25 10:48:13 +0800

    feat: 在照片页面中引入动画和过渡效果，优化图片展示逻辑，调整页面布局以提升用户体验

--- app/photos/[id]/page.tsx
+++ app/photos/[id]/page.tsx
@@ -3,7 +3,10 @@ import { notFound } from "next/navigation"
 import { getNotionPageData } from "@/lib/notion";
 import { ExtendedRecordMap } from "notion-types";
 import { idToUuid } from "notion-utils";
-import PhotoCarousel from "@/app/photos/[id]/photo-carousel";
+import Image from "next/image";
+import FadeIn from "@/components/fade-in";
+import PageTransition from "@/components/page-transition";
+import AnimatedCD from "@/components/animated-cd";
 
 // 定义照片类型
 export interface Photo {
@@ -70,15 +73,29 @@ export default async function PhotoPage({ params }: { params: Promise<{ id: stri
   }
 
   return (
-    <>
+    <PageTransition>
       <Head>
-        <title>Wesley's Photos</title>
+        <title>Wesley's Photo {photo.title}</title>
         <meta property="og:image" content={photo.pageCover} />
         <meta name="twitter:image" content={photo.pageCover} />
       </Head>
       <main className="mx-auto max-w-[1960px] p-4">
-        <PhotoCarousel photo={photo}/>
+        <div className="fixed top-0 left-0 w-screen h-screen backdrop-blur-sm">
+          <div className="absolute -top-4 left-4 z-10">
+            <AnimatedCD href="/photos" />
+          </div>
+          <FadeIn>
+            <Image
+              src={photo?.pageCover ?? ''}
+              fill
+              objectFit="contain"
+              priority
+              alt={photo?.title ?? ''}
+              className="pointer-events-none"
+            />
+          </FadeIn>
+        </div>
       </main>
-    </>
+    </PageTransition>
   );
 };
